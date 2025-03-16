625b64f309b4f74265cb1bc1c3a68d0675cdd4ba
IvanDerbugov <i.derbugoff2001@gmail.com~>
2025-03-16 17:13:11 +0300

    модальное сделано на 70%. js завёрнут в функцию, далее функцию нужно будет сделать универсальной для других модальных окн

--- homework/QUESTIONS/work with modal window/scripts/script.js
+++ homework/QUESTIONS/work with modal window/scripts/script.js
@@ -1,16 +1,35 @@
-const buttonElem = document.querySelector('.section__button')
-const modalElem = document.querySelector('.modal')
+ const modalController = () => {
+    const buttonElems = document.querySelectorAll('.section__button')
+    const modalElem = document.querySelector('.modal')
 
-modalElem.style.cssText = `
-display: flex;
-visibility: hidden;
-opacity: 0;
-transition: opacity 300ms ease-in-out;
-`;
+    modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 300ms ease-in-out;
+    `;
 
-const openModel = () => {
-modalElem.style.visibility = 'visible';
-modalElem.style.opacity = 1;
-}
+    const closeModel = event => {
+    const target = event.target; //хрен его знает что за event
 
-buttonElem.addEventListener('click', openModel)
+    if(target === modalElem || target.closest('.modal__close')) { //хрен его знает что за target.closest
+        modalElem.style.opacity = 0;
+
+        setTimeout(() => {
+            modalElem.style.visibility = 'hidden';
+        }, 300)
+    }
+    }
+
+    const openModel = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+    }
+
+    buttonElems.forEach(btn =>{
+    btn.addEventListener('click', openModel);
+    });
+    modalElem.addEventListener('click', closeModel);
+ }
+
+ modalController()
