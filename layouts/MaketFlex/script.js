eb3472db88157ec7eebc78459f9caed3f8ac38bd
IvanDerbugov <i.derbugoff2001@gmail.com>
2025-06-02 02:21:13 +0300

    Implemented form button type changes to reset for subscription and order buttons. Enhanced mobile menu functionality with click events for opening and closing. Added modal display logic for thank you message upon button clicks. Introduced new cardComfort3 page with associated styles and images for improved product presentation.

--- layouts/MaketFlex/script.js
+++ layouts/MaketFlex/script.js
@@ -3,20 +3,24 @@ const menuMobile = document.querySelector('.menuMobile');
 const close = document.querySelector('#close');
 const menuItems = document.querySelectorAll('.menuMobile ul li');
 
+// Открытие меню по кнопке-гамбургеру
 btnMenuButton.onclick = () => {
     menuMobile.style.display = 'block';
 }
 
+// Закрытие по крестику
 close.onclick = () => {
     menuMobile.style.display = 'none';
 }
 
+// Закрытие по клику на любой пункт меню
 menuItems.forEach(item => {
     item.onclick = () => {
         menuMobile.style.display = 'none'
     }
 })
 
+// Закрытие по клику вне меню и вне кнопки-гамбургера
 document.addEventListener('click', function(event) {
     if (menuMobile.style.display === 'block') {
         if (!menuMobile.contains(event.target) && !btnMenuButton.contains(event.target)) {
@@ -31,7 +35,7 @@ document.addEventListener('click', function(event) {
 const dNoneMobile = document.querySelector('.dNoneMobile');
 const dBlockMobile = document.querySelector('.dBlockMobile');
 const seeMoreMobile = document.querySelector('.seeMoreMobile');
-
+// Открытие и закрытие блока с текстом
 seeMoreMobile.onclick = () => {
     if (dNoneMobile.style.display === 'block') {
         dNoneMobile.style.display = 'none';
@@ -51,22 +55,26 @@ seeMoreMobile.onclick = () => {
 
 
 const modalThanks = document.querySelector('.modalThanks');
-const getDiscount = document.querySelector('.getDiscount');
+const BTNReference = document.querySelectorAll('.BTNReference');
 const closeModalThanks = document.querySelector('.modalThanks button');
 const overlay = document.querySelector('.overlay');
+// Открытие модального окна СПАСИБО
+BTNReference.forEach(btn => {
+    btn.onclick = (event) => {
+        event.stopPropagation();
+        modalThanks.style.display = 'flex';
+        overlay.style.display = 'block';
+    }
+})
 
-getDiscount.onclick = (event) => {
-    event.stopPropagation();
-    modalThanks.style.display = 'flex';
-    overlay.style.display = 'block';
-}
-
+// Закрытие модального окна СПАСИБО
 closeModalThanks.onclick = (event) => {
     event.stopPropagation();
     modalThanks.style.display = 'none';
     overlay.style.display = 'none';
 }
 
+// Закрытие модального окна СПАСИБО по клику вне окна и вне кнопки закрытия
 // document.addEventListener('click', function(event) {
 //     if (modalThanks.style.display === 'block') {
 //         if (!modalThanks.contains(event.target) && !closeModalThanks.contains(event.target)) {
@@ -85,21 +93,25 @@ const btnLeft = document.querySelector('.BtnLeft');
 const btnRight = document.querySelector('.BtnRight');
 let current = 0;
 
+// Показывает отзывы
 function showReview(index) {
     reviews.forEach((r, i) => r.classList.toggle('active', i === index));
     dots.forEach((d, i) => d.style.background = i === index ? '#937A60' : '#fff')
 }    
 
+// Слева
 btnLeft.onclick = () => {
     current = (current -1 + reviews.length) % reviews.length;
     showReview(current);
 }
 
+// Справа
 btnRight.onclick = () => {
     current = (current + 1) % reviews.length;
     showReview(current);
 }
 
+// Показывает отзывы
 dots.forEach((dots, i) =>{
     dots.onclick = () => {
         current = i;
