dbe1be497fa9bd1184eaa36b1b5bc056f3aa0463
IvanDerbugov <i.derbugoff2001@gmail.com>
2025-06-13 12:01:02 +0300

    бургер исправлен

--- layouts/MaketNFTs/js/burgerMenu.js
+++ layouts/MaketNFTs/js/burgerMenu.js
@@ -1,30 +1,62 @@
+// const btnMenuButton = document.querySelector('.burgerMenuMobile');
+// const menuMobile = document.querySelector('.headerTopRight');
+// const close = document.querySelector('#close');
+// const menuItems = document.querySelectorAll('.elMenu');
+
+// // Открытие меню по кнопке-гамбургеру
+// btnMenuButton.onclick = () => {
+//     menuMobile.style.display = 'block';
+// }
+
+// // Закрытие по крестику
+// close.onclick = () => {
+//     menuMobile.style.display = 'none';
+// }
+
+// // Закрытие по клику на любой пункт меню
+// menuItems.forEach(item => {
+//     item.onclick = () => {
+//         menuMobile.style.display = 'none'
+//     }
+// })
+
+// // Закрытие по клику вне меню и вне кнопки-гамбургера
+// document.addEventListener('click', function(event) {
+//     if (menuMobile.style.display === 'block') {
+//         if (!menuMobile.contains(event.target) && !btnMenuButton.contains(event.target)) {
+//             menuMobile.style.display = 'none';
+//         }
+//     }
+// })
+
 const btnMenuButton = document.querySelector('.burgerMenuMobile');
 const menuMobile = document.querySelector('.headerTopRight');
 const close = document.querySelector('#close');
 const menuItems = document.querySelectorAll('.elMenu');
 
+
 // Открытие меню по кнопке-гамбургеру
 btnMenuButton.onclick = () => {
-    menuMobile.style.display = 'block';
+    menuMobile.classList.add('active')
 }
 
 // Закрытие по крестику
 close.onclick = () => {
-    menuMobile.style.display = 'none';
+    menuMobile.classList.remove('active')
 }
 
 // Закрытие по клику на любой пункт меню
 menuItems.forEach(item => {
     item.onclick = () => {
-        menuMobile.style.display = 'none'
+        menuMobile.classList.remove('active')
     }
 })
 
 // Закрытие по клику вне меню и вне кнопки-гамбургера
 document.addEventListener('click', function(event) {
-    if (menuMobile.style.display === 'block') {
+    if (menuMobile.classList.contains('active')) {
         if (!menuMobile.contains(event.target) && !btnMenuButton.contains(event.target)) {
-            menuMobile.style.display = 'none';
+            menuMobile.classList.remove('active')
         }
     }
 })
--- layouts/MaketNFTs/js/timer.js
+++ layouts/MaketNFTs/js/timer.js
@@ -1,39 +1,5 @@
-// let totalSeconds = 59 * 3600 + 59 * 60 + 59;
-
-// const hoursEl = document.getElementById('hours');
-// const minutesEl = document.getElementById('minutes');
-// const secondsEl = document.getElementById('seconds');
-
-// function updateTimer() {
-//     let hours = Math.floor(totalSeconds / 3600); //поделили на часы, получили количествово часов, остаток отбросили
-//     let minutes = Math.floor((totalSeconds % 3600) / 60); //поделили на часы и что не поделилось - минуты
-//     let seconds = totalSeconds % 60; //количество секунд, оставшихся после вычитания полных минут
-
-
-//     hoursEl.textContent = String(hours).padStart(2, '0') //если длинна строки меньше 2 символов - добавляем в начало 0
-//     minutesEl.textContent = String(minutes).padStart(2, '0')
-//     secondsEl.textContent = String(seconds).padStart(2, '0')
-// }
-
-
-// function tick() {
-//     if (totalSeconds > 0) {
-//         totalSeconds--;
-//         updateTimer();
-//     } else {
-//         clearInterval(timerInterval);
-//     }
-// }
-
-// updateTimer(); // начальная отрисовка
-// const timerInterval = setInterval(tick, (1 *1000));
-
-
-
-
 const deadline = new Date('2025-06-19T21:30:00');
 
-// Получаем элементы
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
@@ -42,11 +8,11 @@ function updateTimer() {
     const now = new Date();
     let totalSeconds = Math.floor((deadline - now) / 1000);
 
-    if (totalSeconds < 0) totalSeconds = 0;
+    if (totalSeconds < 0) {totalSeconds = 0};
 
-    let hours = Math.floor(totalSeconds / 3600);
-    let minutes = Math.floor((totalSeconds % 3600) / 60);
-    let seconds = totalSeconds % 60;
+    let hours = Math.floor(totalSeconds / 3600); //поделили на часы, получили количествово часов, остаток отбросили
+    let minutes = Math.floor((totalSeconds % 3600) / 60); //поделили на часы и что не поделилось - минуты
+    let seconds = totalSeconds % 60; //количество секунд, оставшихся после вычитания полных минут
 
     hoursEl.textContent = String(hours).padStart(2, '0');
     minutesEl.textContent = String(minutes).padStart(2, '0');
