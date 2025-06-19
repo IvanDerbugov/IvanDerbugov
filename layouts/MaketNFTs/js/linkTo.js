19a93fabf75e8ddf9c623245d8fd75b93924e127
IvanDerbugov <i.derbugoff2001@gmail.com>
2025-06-19 16:25:28 +0300

    незначительное

--- layouts/MaketNFTs/js/linkTo.js
+++ layouts/MaketNFTs/js/linkTo.js
@@ -1,40 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.linkToSignUp').forEach(function(btn) {
-        btn.addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.linkToSignUp').forEach(function (btn) {
+        btn.addEventListener('click', function () {
             window.open('signUp.html', '_blank');
         });
     });
 
-    document.querySelectorAll('.linkToRankings').forEach(function(btn) {
-        btn.addEventListener('click', function() {
+    document.querySelectorAll('.linkToRankings').forEach(function (btn) {
+        btn.addEventListener('click', function () {
             window.open('topCreators.html', '_blank');
         });
     });
 
-    document.querySelectorAll('.linkToMarketplace').forEach(function(btn) {
-        btn.addEventListener('click', function() {
+    document.querySelectorAll('.linkToMarketplace').forEach(function (btn) {
+        btn.addEventListener('click', function () {
             window.open('marketplace.html', '_blank');
         });
     });
 });
 
 
-document.querySelectorAll('.linkToMetamask').forEach(function(btn) {
-    btn.addEventListener('click', function() {
+document.querySelectorAll('.linkToMetamask').forEach(function (btn) {
+    btn.addEventListener('click', function () {
         window.open('https://metamask.io/ru', '_blank');
     });
 });
 
 
-document.querySelectorAll('.linkToWalletConnect').forEach(function(btn) {
-    btn.addEventListener('click', function() {
+document.querySelectorAll('.linkToWalletConnect').forEach(function (btn) {
+    btn.addEventListener('click', function () {
         window.open('https://walletconnect.network/', '_blank');
     });
 });
 
 
-document.querySelectorAll('.linkToCoinbase').forEach(function(btn) {
-    btn.addEventListener('click', function() {
+document.querySelectorAll('.linkToCoinbase').forEach(function (btn) {
+    btn.addEventListener('click', function () {
         window.open('https://www.coinbase.com/en-gb', '_blank');
     });
 });
