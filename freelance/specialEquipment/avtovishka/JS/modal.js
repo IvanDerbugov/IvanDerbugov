75c269ce92135a5cb2a58afb2f5cf80fd00b7216
IvanDerbugov <i.derbugoff2001@gmail.com>
2025-07-17 01:25:17 +0300

    сделал кусокпланшетки по свадьбам и метрику для рекордики

--- freelance/specialEquipment/avtovishka/JS/modal.js
+++ freelance/specialEquipment/avtovishka/JS/modal.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
     modalWindow.innerHTML = `
         <button class="modal-close" title="Закрыть">&times;</button>
         <h2>Оставьте свои контакты и зафиксируйте свою скидку</h2>
-        <form class="modal-form" action="https://api.web3forms.com/submit" id="formaAvtokrany" method="POST">
+        <form class="modal-form" action="https://api.web3forms.com/submit" id="formaAvtovyschky" method="POST">
             <input type="hidden" name="access_key" value="d759a276-7f88-4a85-a572-a472510fd51b">
             <input type="text" name="name" placeholder="Ваше имя" required autocomplete="name">
             <input type="text" name="contact" placeholder="Телефон или e-mail" required autocomplete="tel">
@@ -100,8 +100,8 @@ document.addEventListener('DOMContentLoaded', function () {
             if (data.success) {
                 // Отправка события в Яндекс.Метрику
                 if (typeof ym === 'function') {
-                    ym(103296307, 'reachGoal', 'formaAvtokrany');
-                    console.log('Цель "Отправка формы" отправлена в Яндекс Метрику (formaAvtokrany)');
+                    ym(103296307, 'reachGoal', 'formaAvtovyschky');
+                    console.log('Цель "Отправка формы" отправлена в Яндекс Метрику (formaAvtovyschky)');
                 }
                 form.style.display = 'none';
                 success.textContent = 'Заявка отправлена!';
